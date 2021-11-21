7000aa4d95dd19971663c014b69854ec72385a52
Bijon Sharma <bijoninfo@gmail.com>
2021-11-21 11:08:03 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/suhan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.suhan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/jahid.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Jahid extends StatefulWidget {
+  const Jahid({Key? key}) : super(key: key);
+
+  @override
+  _JahidState createState() => _JahidState();
+}
+
+class _JahidState extends State<Jahid> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
